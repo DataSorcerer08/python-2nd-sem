4e5122bc65ed1f5f3912ab599a4011896c8cfd80
Ayush Dhruw <166432998+DataSorcerer08@users.noreply.github.com>
2024-05-12 16:04:44 +0530

    Add files via upload

---
+++ self practice/recursion.py
@@ -0,0 +1,9 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+# Example usage:
+number = 5
+print("Factorial of", number, "is", factorial(number))
